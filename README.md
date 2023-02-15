09d2a77b5da9197cbef8e0097c21cbd26a8d8275
Natalia Grzywalska <n.grzywalska@gmail.com>
2023-02-15 11:59:26 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# domain_driven_design_note_app
+
---
+++ android/app/src/main/kotlin/com/example/domain_driven_design_note_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.domain_driven_design_note_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
